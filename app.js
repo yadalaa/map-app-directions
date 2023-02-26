74391fd16b611e5befe87f19b9268c4342e65ff9
yadalaa <yada_la@cmu.ac.th>
2023-02-26 23:57:03 +0700

    map finish

--- app.js
+++ app.js
@@ -1,91 +1,22 @@
-// default map layer
-let map = L.map('map', {
-    layers: MQ.mapLayer(),
-    center: [18.79782216079072, 98.96097433996665],
-    zoom: 15
+var map = L.map('map').setView([18.8027230945381, 98.968252287072], 11);
+mapLink = "<a href='http://openstreetmap.org'>OpenStreetMap</a>";
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: 'Leaflet &copy; ' + mapLink + ', contribution', maxZoom: 18 }).addTo(map);
+
+var Icon = L.icon({
+    iconUrl: 'img/Ramenlogo.png',
+    iconSize: [70, 70]
+})
+
+var marker = L.marker([18.8027230945381, 98.968252287072], { icon: Icon }).addTo(map);
+
+map.on('click', function (e) {
+    console.log(e)
+    var newMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    L.Routing.control({
+        waypoints: [
+            L.latLng(18.8027230945381, 98.968252287072),
+            L.latLng(e.latlng.lat, e.latlng.lng)
+        ]
+
+    }).addTo(map);
 });
-    
-
-    function runDirection(start, end) {
-        
-        // recreating new map layer after removal
-        map = L.map('map', {
-            layers: MQ.mapLayer(),
-            center: [18.79782216079072, 98.96097433996665],
-            zoom: 15
-        });
-        
-        var dir = MQ.routing.directions();
-
-        dir.route({
-            locations: [
-                start,
-                end
-            ]
-        });
-    
-
-        CustomRouteLayer = MQ.Routing.RouteLayer.extend({
-            createStartMarker: (location) => {
-                var custom_icon;
-                var marker;
-
-                custom_icon = L.icon({
-                    iconUrl: 'img/red.png',
-                    iconSize: [20, 29],
-                    iconAnchor: [10, 29],
-                    popupAnchor: [0, -29]
-                });
-
-                marker = L.marker([18.79782216079072, 98.96097433996665], {icon: custom_icon}).addTo(map);
-
-                return marker;
-            },
-
-            createEndMarker: (location) => {
-                var custom_icon;
-                var marker;
-
-                custom_icon = L.icon({
-                    iconUrl: 'img/blue.png',
-                    iconSize: [20, 29],
-                    iconAnchor: [10, 29],
-                    popupAnchor: [0, -29]
-                });
-
-                marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
-
-                return marker;
-            }
-        });
-        
-        map.addLayer(new CustomRouteLayer({
-            directions: dir,
-            fitBounds: true
-        })); 
-    }
-
-
-// function that runs when form submitted
-function submitForm(event) {
-    event.preventDefault();
-
-    // delete current map layer
-    map.remove();
-
-    // getting form data
-    start = document.getElementById("start").value;
-    end = document.getElementById("destination").value;
-
-    // run directions function
-    runDirection(start, end);
-
-    // reset form
-    document.getElementById("form").reset();
-}
-
-// asign the form to form variable
-const form = document.getElementById('form');
-
-// call the submitForm() function when submitting the form
-form.addEventListener('submit', submitForm);
